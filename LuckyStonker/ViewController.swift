220c8f0ab2619a1d4836298aaf5df4e984f8566f
Dmitriy Nikulin <nikdim_03@mail.ru>
2022-03-03 23:04:46 +0300

    Initial Commit

---
+++ LuckyStonker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LuckyStonker
+//
+//  Created by Dmitriy on 3/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
